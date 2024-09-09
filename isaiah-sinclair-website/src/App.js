21594619d66f81bad6c28ff6a7feb910375545cd
Isss11 <isaiah.sinclair.0711@gmail.com>
2024-09-09 10:49:00 -0400

    Add Work Term Three Page

--- isaiah-sinclair-website/src/App.js
+++ isaiah-sinclair-website/src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header.js';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header.js";
 import { Routes, Route, HashRouter } from "react-router-dom";
-import Home from './pages/index';
-import FeaturedProjects from './pages/featuredProjects';
-import Resume from './pages/resume';
-import WorkTermOne from './pages/workTermOne';
-import WorkTermTwo from './pages/workTermTwo';
-import StandardFooter from './components/StandardFooter';
+import Home from "./pages/index";
+import Resume from "./pages/resume";
+import WorkTermOne from "./pages/workTermOne";
+import WorkTermTwo from "./pages/workTermTwo";
+import WorkTermThree from "./pages/workTermThree";
+import StandardFooter from "./components/StandardFooter";
 
 function App() {
-	return (
-		// Routing for website, order of routing in this file does not determine link order.
-		<HashRouter>
-			<Header></Header>
-			<Routes>
-				{/* Empty path should map to initial search of isaiahsinclair.ca */}
-				<Route exact path='' element={<Home />} />
-				<Route exact path='/' element={<Home />} />
-				<Route path='/featuredProjects' element={<FeaturedProjects />} />
-				<Route path='/resume' element={<Resume />} />
-				<Route path='/co-op/s23WorkTerm' element={<WorkTermOne />} />
-				<Route path='/co-op/f23WorkTerm' element={<WorkTermTwo />} />
-			</Routes>
-			<StandardFooter></StandardFooter>
-		</HashRouter>
-	);
+  return (
+    // Routing for website, order of routing in this file does not determine link order.
+    <HashRouter>
+      <Header />
+      <Routes>
+        {/* Empty path should map to initial search of isaiahsinclair.ca */}
+        <Route exact path="" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/co-op/s23WorkTerm" element={<WorkTermOne />} />
+        <Route path="/co-op/f23WorkTerm" element={<WorkTermTwo />} />
+        <Route path="/co-op/s24WorkTerm" element={<WorkTermThree />} />
+      </Routes>
+      <StandardFooter />
+    </HashRouter>
+  );
 }
 
 export default App;
--- isaiah-sinclair-website/src/pages/resume.js
+++ isaiah-sinclair-website/src/pages/resume.js
@@ -1,14 +1,19 @@
 import React from "react";
-import resume from '../assets/isaiahSinclairResume.pdf';
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
+import resume from "../assets/isaiahSinclairResume.pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 const Resume = () => {
-	return (
-		<div>
-			<iframe src={resume} width={"100%"} height={"800rem"} title="Isaiah's Resume"></iframe>
-		</div>
-	);
+  return (
+    <div>
+      <iframe
+        src={resume}
+        width={"100%"}
+        height={"800rem"}
+        title="Isaiah's Resume"
+      />
+    </div>
+  );
 };
 
 export default Resume;
